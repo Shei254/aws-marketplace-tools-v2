e3eef4c36a4567c5f323e8646039aec0413444a2
shei254 <chegeeric66@gmail.com>
2024-07-09 02:52:51 +0300

    foreign keys update on aws subscription table

--- src/database/migrations/2024_06_14_222939_create_aws_subscriptions_table.php
+++ src/database/migrations/2024_06_14_222939_create_aws_subscriptions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('aws_subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger("aws_customer_id");
+            $table->string("aws_customer_id");
             $table->string("dimension");
             $table->unsignedInteger("quantity")->default(0);
             $table->timestamps();
